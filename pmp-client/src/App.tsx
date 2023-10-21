c4c6d36a2e7d012f210d15d2720d33dfdb4dcb02
bliztle <asbjoern.r.e@gmail.com>
2023-10-21 02:55:15 +0200

    global error handling

--- pmp-client/src/App.tsx
+++ pmp-client/src/App.tsx
@@ -10,52 +10,57 @@ import SignOut from './routes/SignOut/SignOut';
 
 const router = createBrowserRouter([
     {
-        path: '/signin',
-        element: <SignInPage />
-    },
-    {
-        path: '/signout',
-        element: <SignOut />
-    },
-    {
         path: '/',
-        loader: () => {
-            return redirect(`/${Environment.INVALID}/parameters`);
-        }
-    },
-    {
-        path: '/:environment/',
-        element: <Layout />,
         errorElement: <ErrorPage />,
-        loader: ({ request, params }) => {
-            if (!params.environment) {
-                // This should never happen. Can only occour if route is misconfigured
-                throw new Error('Matched environment route with no environment param');
-            }
-
-            // None of the below routes will match if environment is not followed by another path segment,
-            if (/^(https?):\/\/[^/]+\/[^/]+\/?$/.test(request.url)) {
-                throw new Response('', {
-                    status: 404,
-                    statusText: 'Not Found'
-                });
-            }
-
-            // Redirect to Enviroment.INVALID if environment is not recognized
-            if (!ENVIRONMENTS.includes(params.environment as Environment)) {
-                return redirect(request.url.replace(params.environment, Environment.INVALID));
-            }
-
-            return null;
-        },
         children: [
             {
-                path: 'parameters',
-                element: <ParametersPage />
+                path: '/',
+                loader: () => {
+                    return redirect(`/${Environment.INVALID}/parameters`);
+                }
             },
             {
-                path: 'audit',
-                element: <AuditPage />
+                path: '/signin',
+                element: <SignInPage />
+            },
+            {
+                path: '/signout',
+                element: <SignOut />
+            },
+            {
+                path: '/:environment/',
+                element: <Layout />,
+                loader: ({ request, params }) => {
+                    if (!params.environment) {
+                        // This should never happen. Can only occour if route is misconfigured
+                        throw new Error('Matched environment route with no environment param');
+                    }
+
+                    // None of the below routes will match if environment is not followed by another path segment,
+                    if (/^(https?):\/\/[^/]+\/[^/]+\/?$/.test(request.url)) {
+                        throw new Response('', {
+                            status: 404,
+                            statusText: 'Not Found'
+                        });
+                    }
+
+                    // Redirect to Enviroment.INVALID if environment is not recognized
+                    if (!ENVIRONMENTS.includes(params.environment as Environment)) {
+                        return redirect(request.url.replace(params.environment, Environment.INVALID));
+                    }
+
+                    return null;
+                },
+                children: [
+                    {
+                        path: 'parameters',
+                        element: <ParametersPage />
+                    },
+                    {
+                        path: 'audit',
+                        element: <AuditPage />
+                    }
+                ]
             }
         ]
     }
