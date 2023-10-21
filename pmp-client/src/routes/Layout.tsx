3e76257cc2e45f22dac0f91745cc653040cfd8d2
bliztle <asbjoern.r.e@gmail.com>
2023-10-21 10:27:17 +0200

    added map example

--- pmp-client/src/routes/Layout.tsx
+++ pmp-client/src/routes/Layout.tsx
@@ -1,5 +1,5 @@
 import { Outlet, useLocation, useNavigate } from 'react-router-dom';
-import useEnvironment, { Environment } from '../hooks/useEnvironment';
+import useEnvironment, { Environment, VALID_ENVIRONMENTS } from '../hooks/useEnvironment';
 
 const Layout = () => {
     const { environment, isValid } = useEnvironment();
@@ -20,10 +20,18 @@ const Layout = () => {
                 <>
                     <div>
                         <p>To proceed, please pick an environment</p>
-                        <button onClick={() => setEnvironment(Environment.TEST)}>Test</button>
+
+                        {/** This expression is similar to the commented one below, automatically generating the buttons fron the list of valid environments */}
+                        {VALID_ENVIRONMENTS.map((env) => (
+                            <button className='bg-gray-300 rounded-3xl pl-2 pr-2' onClick={() => setEnvironment(env)}>
+                                {env.charAt(0).toUpperCase() + env.slice(1)}
+                            </button>
+                        ))}
+
+                        {/* <button onClick={() => setEnvironment(Environment.TEST)}>Test</button>
                         <button onClick={() => setEnvironment(Environment.DEV)}>Dev</button>
                         <button onClick={() => setEnvironment(Environment.PREPROD)}>Preprod</button>
-                        <button onClick={() => setEnvironment(Environment.PROD)}>Prod</button>
+                        <button onClick={() => setEnvironment(Environment.PROD)}>Prod</button> */}
                     </div>
                 </>
             )}
