a1e93271433d93260cb49abe0ca8ce8fc4f3f339
bliztle <asbjoern.r.e@gmail.com>
2023-10-22 01:30:43 +0200

    documented queries

--- pmp-client/src/features/network/query.tsx
+++ pmp-client/src/features/network/query.tsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
 
+/** Wrapper for QueryClientProvider */
 export const QueryProvider = ({ children }: { children: React.ReactNode }) => {
     return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 };
--- pmp-client/src/features/network/useSimpleQuery.ts
+++ pmp-client/src/features/network/useSimpleQuery.ts
@@ -8,6 +8,14 @@ type UseSimpleQuery = {
     <TParser extends z.ZodType>(queryKey: QueryKey, url: string, parser: TParser): UseQueryResult<z.infer<TParser>>;
 };
 
+/**
+ * An abstraction over react-query's useQuery hook, which makes it easier to make simple, validated get requests
+ * @param queryKey The query key to use for the request. This is used to identify the request in react-query's cache.
+ * @param url The URL to make the request to.
+ * @param parser An optional parser to use to validate the response. If not provided, the response will not be validated.
+ *
+ * @returns A UseQueryResult object, identical to the one returned by react-query's useQuery hook.
+ */
 const useSimpleQuery = (<TData>(queryKey: QueryKey, url: string, parser?: z.ZodType<TData>) => {
     return useQuery({
         queryKey,
