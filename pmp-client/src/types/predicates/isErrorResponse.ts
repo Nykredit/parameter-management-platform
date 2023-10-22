6e77d34f5900ac9fe519878f0732317c456511fb
bliztle <asbjoern.r.e@gmail.com>
2023-10-22 02:26:02 +0200

    docs

--- pmp-client/src/types/predicates/isErrorResponse.ts
+++ pmp-client/src/types/predicates/isErrorResponse.ts
@@ -1,5 +1,10 @@
 import { ErrorResponse } from 'react-router-dom';
 
+/**
+ * Checks if the given error is a react router ErrorResponse object.
+ * @param error The error to check.
+ * @returns True if the error is an ErrorResponse object.
+ */
 const isErrorResponse = (error: ErrorResponse | Error): error is ErrorResponse => {
     return (error as ErrorResponse).status !== undefined;
 };
--- pmp-client/src/utils/string.ts
+++ pmp-client/src/utils/string.ts
@@ -1,3 +1,4 @@
+/** Capitalises the first letter of a string */
 export const capitaliseFirstLetter = (str: string) => {
     return str.charAt(0).toUpperCase() + str.slice(1);
 };
