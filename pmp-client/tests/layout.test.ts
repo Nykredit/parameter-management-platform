b9cf0c9fe0d25a2d6d343b093a31e2667ab098fe
Bliztle <asbjoern.r.e@gmail.com>
2023-10-23 21:48:36 +0200

    test cleanup

--- pmp-client/tests/layout.test.ts
+++
@@ -1,9 +0,0 @@
-import { expect, test } from '@playwright/test';
-
-test('displays current environment in header', async ({ page }) => {
-    const environment = 'prod';
-    // await page.goto(`http://localhost:5173/${environment}/parameters`);
-    await page.goto(`/${environment}/parameters`);
-
-    await expect(page.locator('header').getByText(environment)).toBeVisible();
-});
--- pmp-client/tests/routing.test.ts
+++
@@ -1,9 +0,0 @@
-import { expect, test } from '@playwright/test';
-
-test('corrects invalid environments', async ({ page }) => {
-    const environment = 'this is not a valid environment';
-    await page.goto(`/${environment}/parameters`);
-
-    const url = new URL(page.url());
-    expect(url.pathname).toBe('/invalid/parameters');
-});
