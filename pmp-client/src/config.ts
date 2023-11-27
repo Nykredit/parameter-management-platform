3d7cee419f02fa1ec8cc07e799ba06e415ab27e8
Bliztle <asbjoern.r.e@gmail.com>
2023-11-27 23:33:58 +0100

    dynamic redirect uri. Works on all approved domains

--- pmp-client/src/config.ts
+++ pmp-client/src/config.ts
@@ -1,6 +1,6 @@
-/** Redirect url base for AAD authentication. Used for both signin and singout */
+/** Redirect url base for AAD authentication. Used for both signin and singout. @default window.location.origin */
 export const AAD_REDIRECT_URI_BASE: string =
-    (import.meta.env.VITE_AAD_REDIRECT_URI_BASE as string) ?? 'http://localhost:5173';
+    (import.meta.env.VITE_AAD_REDIRECT_URI_BASE as string) ?? window.location.origin;
 
 export const AAD_CLIENT_ID: string =
     (import.meta.env.VITE_AAD_CLIENT_ID as string) ?? 'f74350bc-51a5-4955-8fac-859d8cca13df';
