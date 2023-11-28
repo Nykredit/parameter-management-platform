e2b3d0e7cf6b57d878b9cd6188ebfa7ff6969bfe
Bliztle <84600908+Bliztle@users.noreply.github.com>
2023-11-28 09:46:33 +0100

    Added text if no service is selected (#41)

--- pmp-client/src/routes/parameters/ServiceList.tsx
+++ pmp-client/src/routes/parameters/ServiceList.tsx
@@ -1,17 +1,23 @@
 import ServiceListElement from './ServiceListElement';
+import { Typography } from 'rmwc';
 import useSelectedServices from '../../features/services/useSelectedServices';
 
 const ServiceList = () => {
-	const [services] = useSelectedServices();
-	const sortedServices = services.sort((a, b) => a.name.localeCompare(b.name));
+    const [services] = useSelectedServices();
+    const sortedServices = services.sort((a, b) => a.name.localeCompare(b.name));
 
-	return (
-		<>
-			{sortedServices.map((service) => (
-				<ServiceListElement key={service.address} service={service} />
-			))}
-		</>
-	);
+    return (
+        <>
+            {sortedServices.map((service) => (
+                <ServiceListElement key={service.address} service={service} />
+            ))}
+            {sortedServices.length === 0 && (
+                <div className='mt-2'>
+                    <Typography use='subtitle1'>Choose a service from the list in the left drawer</Typography>
+                </div>
+            )}
+        </>
+    );
 };
 
 export default ServiceList;
